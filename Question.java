dbe0750dc7484e34d99c4c55d924be5d5dbfa82f
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-06-13 10:46:10 +0200

    Update Question.java

--- Question.java
+++ Question.java
@@ -2,23 +2,33 @@ public class Question {
 
     public String mQuestions[]=
             {
-                    "Quelle est la capitale de la Roumanie ?",
+                    "Quelle est la capitale de l'Allemagne ?",
                     "Quelle est la capitale de l'Hongrie ?",
                     "Quelle est la capitale de la Lituanie ?",
-                    "Quelle est la capitale de la Slovaquie ?",
-                    "Quelle est la capitale de la Slovénie ?"
+                    "Quelle est la capitale de la France ?",
+                    "Quelle est la capitale de la Slovénie ?",
+                    "Quelle est la capitale de la Belgique ?",
+                    "Quelle est la capitale du Portugal ?",
+                    "Quelle est la capitale de l'Italie ?",
+                    "Quelle est la capitale du Danemark ?",
+                    "Quelle est la capitale de l'Autriche ?"
             };
 
     private String mChoices[][]=
             {
-                {"Paris", "Bucarest" , "Vienne" , "Lisbonne"},
-                {"Paris" , "Bucarest" ,"Budapest"  ,"Zurich"},
-                {"Rome" , "Vilnius" ,"Vienne"  ,"Amsterdam"},
-                {"Bratislava" , "Bucarest" ,"Budapest" ,"Lisbonne"},
-                {"Bratislava" , "Bucarest" ,"Budapest" ,"Ljubljana"}
+                {"Paris", "Berlin", "Vienne", "Lisbonne"},
+                {"Paris", "Bucarest", "Budapest", "Zurich"},
+                {"Rome", "Vilnius", "Vienne", "Amsterdam"},
+                {"Paris", "Bucarest", "Budapest", "Lisbonne"},
+                {"Bratislava", "Bucarest", "Budapest", "Ljubljana"},
+                {"Bruxelles", "Paris", "Budapest", "Athènes"},
+                {"Bruxelles", "Madrid", "Berlin", "Lisbonne"},
+                {"Paris", "Copenhague", "Budapest", "Rome"},
+                {"Madrid", "Rome", "Bruxelles", "Copenhague"},
+                {"Paris", "Madrid", "Berlin", "Vienne"},
             };
 
-    private String correctanswers[]={"Bucarest","Budapest","Vilnius","Bratislava","Ljubljana"};
+    private String correctanswers[]={"Berlin","Budapest","Vilnius","Paris","Ljubljana","Bruxelles","Lisbonne","Rome","Copenhague","Vienne"};
 
     public String getQuestion(int a){
         String question = mQuestions[a];
@@ -47,6 +57,36 @@ public class Question {
 
     public String getChoice5(int a){
         String choice= mChoices[a][4];
+        return choice;
+    }
+    
+        public String getChoice6(int a){
+        String choice= mChoices[a][5];
+        return choice;
+    }
+    
+        public String getChoice7(int a){
+        String choice= mChoices[a][6];
+        return choice;
+    }
+    
+        public String getChoice8(int a){
+        String choice= mChoices[a][7];
+        return choice;
+    }
+    
+        public String getChoice9(int a){
+        String choice= mChoices[a][8];
+        return choice;
+    }
+    
+        public String getChoice10(int a){
+        String choice= mChoices[a][9];
+        return choice;
+    }
+    
+        public String getChoice11(int a){
+        String choice= mChoices[a][10];
         return choice;
     }
 
