47c0af2ec46c3b4fea7e2e1f2527c18cf2ee17df
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-06-13 10:49:00 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -27,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         answer2 = (Button) findViewById(R.id.answer2);
         answer3 = (Button) findViewById(R.id.answer3);
         answer4 = (Button) findViewById(R.id.answer4);
+        answer5 = (Button) findViewById(R.id.answer5);
+        answer6 = (Button) findViewById(R.id.answer6);
+        answer7 = (Button) findViewById(R.id.answer7);
+        answer8 = (Button) findViewById(R.id.answer8);
+        answer9 = (Button) findViewById(R.id.answer9);
+        answer10 = (Button) findViewById(R.id.answer10);
 
         question = (TextView) findViewById(R.id.question);
         score = (TextView) findViewById(R.id.score);
@@ -82,6 +88,78 @@ public class MainActivity extends AppCompatActivity {
                     gameOver();
             }
         });
+        
+        answer5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
+        
+        answer6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
+        
+        answer7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
+        
+        answer8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
+        
+        answer9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
+        
+        answer10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText()== mAnswer){
+                    mScore++;
+                    score.setText("Score : " + mScore);
+                    updateQuestion(r.nextInt(mQuestionlength));
+                }else
+                    gameOver();
+            }
+        });
     }
 
         private void updateQuestion(int num){
@@ -90,6 +168,12 @@ public class MainActivity extends AppCompatActivity {
             answer2.setText(mQuestions.getChoice2(num));
             answer3.setText(mQuestions.getChoice3(num));
             answer4.setText(mQuestions.getChoice4(num));
+            answer5.setText(mQuestions.getChoice4(num));
+            answer6.setText(mQuestions.getChoice4(num));
+            answer7.setText(mQuestions.getChoice4(num));
+            answer8.setText(mQuestions.getChoice4(num));
+            answer9.setText(mQuestions.getChoice4(num));
+            answer10.setText(mQuestions.getChoice4(num));
             mAnswer = mQuestions.getCorrectAnswer(num);
         }
 
